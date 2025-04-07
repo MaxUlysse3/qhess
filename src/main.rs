124e9c4ccd74d3f5099d5a305b1d0d0399e8d276
Ulysse <ulyssewaintal@gmail.com>
2025-04-07 11:48:35 +0200

    Began implementing the board

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,103 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::Wireframe2d, window::EnabledButtons};
+
+const WIDTH: f64 = 800.;
+const HEIGHT: f64 = 600.;
+
+#[derive(Resource)]
+struct ChessBoardPos {
+    bot_left: (f64, f64),
+    size: f64,
+    square_size: f64,
+    square_pos: Vec<(f64, f64)>, // Row major matrix
+}
+
+impl ChessBoardPos {
+    fn new(bot_left: (f64, f64), size: f64) -> Self {
+        let square_size = size / 8.;
+        let square_pos = (0..64).map(|idx| {
+            let i = idx % 8;
+            let j = idx / 8;
+            (bot_left.1 + (i as f64) * square_size, bot_left.0 + (j as f64) * square_size)
+        }).collect();
+        Self {
+            bot_left,
+            size,
+            square_size,
+            square_pos,
+        }
+    }
+}
+
+#[derive(Resource)]
+struct BoardState {
+    squares: Vec<Square> // Row major matrix
+}
+
+impl Default for BoardState {
+    fn default() -> Self {
+        let base_line = vec![Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook];
+        let pawn_line: Vec<Piece> = std::iter::repeat(Pawn).take(8).collect();
+
+        let all_empty = std::iter::repeat(Square::Empty).take(32).collect::<Vec<_>>();
+
+        let board = base_line.iter()
+            .map(|p| Square::Occupied(*p, White))
+            .chain(pawn_line.iter().map(|p| Square::Occupied(*p, White)))
+            .chain(all_empty)
+            .chain(pawn_line.iter().map(|p| Square::Occupied(*p, Black)))
+            .chain(base_line.iter().map(|p| Square::Occupied(*p, Black)))
+            .collect::<Vec<_>>();
+
+        Self {
+            squares: board,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Piece {
+    Pawn,
+    Bishop,
+    Knight,
+    Rook,
+    Queen,
+    King,
+}
+use Piece::*;
+
+#[derive(Debug, Copy, Clone)]
+enum Team {
+    White,
+    Black,
+}
+use Team::*;
+
+#[derive(Debug, Copy, Clone)]
+enum Square {
+    Empty,
+    Occupied(Piece, Team),
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Quantum Chess".into(),
+                name: Some("qhess".into()),
+                resolution: (WIDHT, HEIGHT).into(),
+                window_theme: Some(bevy::window::WindowTheme::Dark),
+                enabled_buttons: EnabledButtons {
+                    maximize: false,
+                    ..Default::default()
+                },
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(Wireframe2dPlugin),
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands:  Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
-    commands.spawn(Sprite::from_image(
-        asset_server.load("chessboard.png"),
-    ));
 }
